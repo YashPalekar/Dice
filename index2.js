4ebe1728a11e40577714567e02de3c2995f9ead3
Yash Palekar <49448619+YashPalekar@users.noreply.github.com>
2022-09-11 12:42:07 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,9 +1,9 @@
 var randomNumber1 = Math.floor( Math.random() * 6 ) + 1;
-var diceImage1 = "https://github.com/YashPalekar/Dice/blob/2a11d52c9f67d2b4f6b16c44cbb422597a1466cd/images/dice" + randomNumber1 + ".png";
+var diceImage1 = "images/dice" + randomNumber1 + ".png";
 document.querySelectorAll("img")[0].setAttribute( "src", diceImage1 );
 
 var randomNumber2 = Math.floor( Math.random() * 6) + 1;
-var diceImage2 = "https://github.com/YashPalekar/Dice/blob/2a11d52c9f67d2b4f6b16c44cbb422597a1466cd/images/dice" + randomNumber2 + ".png";
+var diceImage2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", diceImage2);
 
 if(randomNumber1 > randomNumber2) { document.querySelector("h1").innerHTML = "🚩PLayer 1 Wins!"; }
